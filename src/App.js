f1aaa2d4f60332cdd1676afd3f399321436096c2
Daisy <daisyliudesign@gmail.com>
2020-12-26 05:17:38 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import Board from "./Board";
+import Music from "./Music";
+import "./App.css";
+
+/** Simple app that just shows the LightsOut game. */
+
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <div className="display">      
+          <Board/>
+        <Music/>
+        </div>  
+        <p className="instruction">Pressing any of the lights will toggle it and the four adjacent lights. The goal of the puzzle is to switch all the lights off, preferably in as few button presses as possible.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
