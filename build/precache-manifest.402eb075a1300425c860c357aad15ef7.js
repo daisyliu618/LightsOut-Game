d32f9e11b156b2248308f7e82fce59e62e61bcc2
Daisy <daisyliudesign@gmail.com>
2020-12-26 17:48:41 -0500

    fix issue

---
+++ build/precache-manifest.402eb075a1300425c860c357aad15ef7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d806ea65b7043836a93",
+    "url": "/static/css/main.015916f2.chunk.css"
+  },
+  {
+    "revision": "9d806ea65b7043836a93",
+    "url": "/static/js/main.9d806ea6.chunk.js"
+  },
+  {
+    "revision": "85ca97b23f4919cc4cf5",
+    "url": "/static/js/1.85ca97b2.chunk.js"
+  },
+  {
+    "revision": "f142cb97dd5e277d0755",
+    "url": "/static/js/2.f142cb97.chunk.js"
+  },
+  {
+    "revision": "185828960f1507369d21",
+    "url": "/static/js/runtime~main.18582896.js"
+  },
+  {
+    "revision": "e70852f0b33ac6b6caab034294df4ae4",
+    "url": "/static/media/flip.e70852f0.mp3"
+  },
+  {
+    "revision": "87e6bd67815defec3f4c135db8ed1eab",
+    "url": "/static/media/goeasy.87e6bd67.mp3"
+  },
+  {
+    "revision": "ed9fefcdc05f4695b65fb605468b0dba",
+    "url": "/static/media/dreams.ed9fefcd.mp3"
+  },
+  {
+    "revision": "26e3010806ef70bb3e1cd122ba3075e0",
+    "url": "/static/media/chillout.26e30108.mp3"
+  },
+  {
+    "revision": "6bf275b2c198cf1a77745eb781a3e2e0",
+    "url": "/static/media/twistter.6bf275b2.mp3"
+  },
+  {
+    "revision": "0740af77a392fb1c28e91accd5d21308",
+    "url": "/static/media/freebird.0740af77.mp3"
+  },
+  {
+    "revision": "fa2785d17195b2689a037990151d23a3",
+    "url": "/static/media/love.fa2785d1.mp3"
+  },
+  {
+    "revision": "d11e76f4d0b724dbdf9edcc48a87dd66",
+    "url": "/static/media/morningdew.d11e76f4.mp3"
+  },
+  {
+    "revision": "254789052bedaccd4be8aa8059c68d97",
+    "url": "/static/media/thelounge.25478905.mp3"
+  },
+  {
+    "revision": "bbe3e8e527f56c81dd0d3dd698b37c44",
+    "url": "/index.html"
+  }
+];
