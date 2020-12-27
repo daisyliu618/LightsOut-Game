30948c8ef1043255f9f3eb0c1a9cd4f56aa1c532
Daisy <daisyliudesign@gmail.com>
2020-12-26 23:10:09 -0500

    add icon

--- build/service-worker.js
+++ build/service-worker.js
@@ -11,11 +11,11 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts(
-    'https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js'
-);
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
-importScripts('/precache-manifest.4cdcc973c4682e4c4831afacfc7ec2ce.js');
+importScripts(
+  "/precache-manifest.81a9cdc56cb9dafc87e5dd5522bdc3b2.js"
+);
 
 workbox.clientsClaim();
 
@@ -28,6 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute('/index.html', {
-    blacklist: [/^\/_/, /\/[^/]+\.[^/]+$/],
+workbox.routing.registerNavigationRoute("/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
